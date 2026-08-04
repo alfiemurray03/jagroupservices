5e64349c25eea2c2bde498f3fad9f6695f88017e
ALFIE THOMAS HOLYWOOD MURRAY <232256186+alfiemurray03@users.noreply.github.com>
2026-08-04 04:58:43 +0100

    Use updated brands page with Aptenvo

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,7 +6,7 @@ import AboutUsPage from './pages/about-us';
 import MeetTheTeamPage from './pages/meet-the-team';
 import JackNicolauSousaDaSilvaPage from './pages/team/jack-nicolau-sousa-da-silva';
 import AlfieThomasHolywoodMurrayPage from './pages/team/alfie-thomas-holywood-murray';
-import AboutOurDivisionsPage from './pages/about-our-divisions';
+import AboutOurDivisionsPage from './pages/brands';
 import OurGroupStructurePage from './pages/our-group-structure';
 import ServicesPage from './pages/services';
 import CustomerSupportPage from './pages/customer-support';
